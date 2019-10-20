f89fe4c051ea789cbaab0d393cc9bb6c323606e5
José Gomera <j.gomera@motorcredito.com.do>
2019-10-20 00:32:12 -0400

    Add Business Interfaces

---
+++ AcquisitionPlus.Business/Interfaces/IBaseRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace AcquisitionPlus.Business.Interfaces
+{
+    public interface IBaseRepository<TEntity>
+    {
+        TEntity Get(Expression<Func<TEntity, bool>> predicate);
+        TEntity Get(int id);
+        IEnumerable<TEntity> GetAll();
+        IEnumerable<TEntity> GetAll(Expression<Func<TEntity, bool>> predicate);
+
+        void Add(TEntity entity);
+        void AddAll(IEnumerable<TEntity> entities);
+
+        void Remove(TEntity entity);
+        void RemoveAll(IEnumerable<TEntity> entities);
+    }
+}
