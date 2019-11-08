d421ed294a590c9117bf88c8cfa006b794101b70
José Gomera <j.gomera@motorcredito.com.do>
2019-11-08 16:49:38 -0400

    add employee list

--- AcquisitionPlus.Web/ClientApp/src/app/manteniminentos/empleados/empleados-routing.module.ts
+++ AcquisitionPlus.Web/ClientApp/src/app/manteniminentos/empleados/empleados-routing.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 import { EmpleadosListComponent } from './list/empleados-list.component';
+import { EmployeeResolver } from 'src/app/core/resolvers/employee.resolver';
 
 
 const routes: Routes = [
-  {path: '', component: EmpleadosListComponent}
+  {path: '', component: EmpleadosListComponent, resolve: {employee: EmployeeResolver }}
 ];
 
 @NgModule({
--- AcquisitionPlus.Web/ClientApp/src/app/manteniminentos/empleados/empleados.module.ts
+++ AcquisitionPlus.Web/ClientApp/src/app/manteniminentos/empleados/empleados.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { MaterialModule } from 'src/app/material/material.module';
 import { EmpleadosListComponent } from './list/empleados-list.component';
 import { EmpleadoRoutingModule } from './empleados-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 
@@ -11,7 +12,8 @@ import { EmpleadoRoutingModule } from './empleados-routing.module';
   imports: [
     CommonModule,
     MaterialModule,
-    EmpleadoRoutingModule
+    EmpleadoRoutingModule,
+    ReactiveFormsModule
   ]
 })
 export class EmpleadosModule { }
