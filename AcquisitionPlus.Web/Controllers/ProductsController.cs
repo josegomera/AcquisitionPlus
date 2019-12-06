6a9f744929b198947dc8c8d5aa4d672841b45c5b
José Gomera <j.gomera@motorcredito.com.do>
2019-12-06 08:10:01 -0400

    filter product out of stock

--- AcquisitionPlus.Web/Controllers/ProductsController.cs
+++ AcquisitionPlus.Web/Controllers/ProductsController.cs
@@ -55,8 +55,10 @@ namespace AcquisitionPlus.Web.Controllers
         {
             try
             {
-                var products = _unitOfWork.Product.GetAll().Select(x => new { x.Id, Product = x.Description, x.Stock}).ToList();
-         
+                var products = _unitOfWork.Product.GetAll()
+                    .Where(s => s.Stock != 0)
+                    .Select(x => new { x.Id, Product = x.Description, x.Stock}).ToList();
+
                 return StatusCode(200, products);
             }
             catch (Exception e)
